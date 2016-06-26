21159950a0ad6abf67ea818d5f36db8480dd1abc
bkvillalobos <brendonv93@gmail.com>
2016-06-26 18:43:37 -0400

    added function to turn pid into indicator

--- calc_util.py
+++ calc_util.py
@@ -7,6 +7,7 @@ from file_constants import RecFileConstants as rc, TestFileConstants as tc
 
 def purchases_to_dict(data, delimiter='\t'):
     """
+    # TODO: write description
     :param data: either pd.DataFrame of customer purhcase data or a string path to delimited file containing data
     :param delimiter: character separating values in delimited file
     :return purchase_dict: dictionary with customer ids as keys, a list of purchases as values
@@ -27,3 +28,25 @@ def purchases_to_dict(data, delimiter='\t'):
         purchase_dict[customer] = list(data[data[tc.CID] == customer][tc.PID])
 
     return purchase_dict
+
+def pid_to_indicator(rec_df, purchase_dict, row_index=0, in_place=True):
+    """
+    # TODO: document
+    :param rec_df:
+    :param purchase_dict:
+    :param row_index:
+    :return:
+    """
+    customer = rec_df[rc.CID]
+    purchases = purchase_dict[customer]
+    curr_vals = rec_df.ix[row_index].values
+    # turns all recommendation ids into purchase indicator, leaves all other  (non-string) df values alone
+    result = [rec if not isinstance(rec,six.string_types)
+              else 1 if rec in purchases
+              else 0
+              for rec in purchases if isinstance(rec, six.string_types)]
+    if in_place:
+        rec_df.ix[row_index] = result
+    else:
+        return result
+
